7c370f8c76710792bec34618345cc2ee1419f9ed
konkon-T <konkon.gitsune628@gmail.com>
2023-02-19 22:16:02 +0900

    Adding message at testing code

--- src/test/java/com/example/rental_app/AbstractApplicationTest.java
+++ src/test/java/com/example/rental_app/AbstractApplicationTest.java
@@ -10,10 +10,15 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+// SpringBootのテストであることを指定する。
 @SpringBootTest(
+        // テストを実行するときには、接続先のPORT番号は、ランダムで行うように設定する
+        // DatabaseのPORT番号やJAVA等のPORTとかぶる可能性を考慮
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+// TestContainerを利用するアノテーション
 @Testcontainers
+// テストのライフサークル設定(*あまり深く考えなくていい。(調べていくと難しいので))
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AbstractApplicationTest {
 
@@ -23,17 +28,22 @@ public abstract class AbstractApplicationTest {
             .withDatabaseName("testdb")
             .withUsername("testUser")
             .withPassword("test");
-    // 通信を行うライブラリー(Test用)
+
+    /* 通信を行うライブラリー(Test用) 継承元すべてで利用することを想定しているため、抽象クラスに定義 */
     @Autowired
     protected WebTestClient webTestClient;
 
+    /* テスト1項目(@Testの記載をカウント数とする。)ごとに実行する前処理 */
     @AfterAll
     protected void start() {
+        // TestContainer(Dockerへの接続開始)
         database.start();
     }
 
+    /* テスト1項目(@Testの記載をカウント数とする。)ごとに実行する後処理 */
     @BeforeAll
     protected void stop() {
+        // TestContainer(Dockerへの接続切断)
         database.stop();
     }
 
--- src/test/java/com/example/rental_app/RentalAppApplicationTests.java
+++ src/test/java/com/example/rental_app/RentalAppApplicationTests.java
@@ -5,17 +5,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-
+/* よく見るテストコードではこちらに@SpringBootTestや@TestContainerの記載が見られるが、
+    継承元であるAbstractApplicationTestで定義しているため、記載する必要性がない。
+* */
 class RentalAppApplicationTests extends AbstractApplicationTest {
 
     @Test
     void IS_TEST_CONTROLLER_ACCEPT_SUCCESS() {
         webTestClient.get()
-                .uri("/test")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
+                .uri("/test") /* ここまでで、POSTMANで通信を行う時と同じようにURLとHTTPMETHOD(GETやPOST)をしている */
+                .exchange() /* 上記のURLへリクエストを投げるものだと考えればいいです。(厳密には違いますが。。) */
+                .expectStatus().isOk() /* expect〇〇 は、レスポンス情報を取得している */
+                .expectBody(String.class) /* expectBody は、レスポンス時に返却されるBean(Response)クラスを指定することで情報の取得を行うことができる。 */
                 .value(message -> assertEquals(message, "OK"));
+                /* Returnされた文字列 を精査している。 assertEqualsは, 前後の引数が正しいかどうか判定。
+                   正しくない場合は、Falseとなり、テストに失敗する。試してみるといい。
+                   value()の()の中はラムダ式の書き方となっていてわかり使いが、下記のようなメソッドだとおもってくれて問題ない。
+
+                    // actualは上記の例だと "OK"
+
+                   boolean 〇〇 (String message, String actual){
+                    return message.equals(actual);
+                   }
+
+                 */
     }
 
     @Test
